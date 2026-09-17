9819f731510bce435f0f5cf4ba8f9933c348895a
PintoJR <guimoraespinto@gmail.com>
2026-09-17 01:23:55 -0300

    Solution03

--- app/main.py
+++ app/main.py
@@ -1,98 +1,10 @@
-from abc import ABC, abstractmethod
-
-
-class Player(ABC):
-    def __init__(self, nickname: str) -> None:
-        self.nickname = nickname
-
-    @abstractmethod
-    def get_rating(self) -> None:
-        pass
-
-    @abstractmethod
-    def player_info(self) -> int:
-        pass
-
-
-class Elf(Player):
-    def __init__(self, nickname: str, musical_instrument: str) -> None:
-        super().__init__(nickname)
-        self._musical_instrument = musical_instrument
-
-    def play_elf_song(self) -> None:
-        print(f"{self.nickname} is playing a song on the \
-              {self._musical_instrument}")
-
-
-class Dwarf(Player):
-    def __init__(self, nickname: str, favourite_dish: str) -> None:
-        super().__init__(nickname)
-        self._favourite_dish = favourite_dish
-
-    def eat_favourite_dish(self) -> None:
-        print(f"{self.nickname} is eating {self._favourite_dish}")
-
-    def player_info(self) -> str:
-        return f'"Elf ranger {self.nickname}. {self.nickname} has bow \
-            of the {self.bow_level} level"` for `ElfRanger` instances"'
-
-
-class ElfRanger(Elf):
-    def __init__(self, nickname: str, musical_instrument: str,
-                 bow_level: int) -> None:
-        super().__init__(nickname, musical_instrument)
-        self._bow_level = bow_level
-
-    def player_info(self) -> str:
-        return f'"Elf ranger {self.nickname}. {self.nickname} has bow \
-            of the {self.bow_level} level"` for `ElfRanger` instances"'
-
-    def get_rating(self) -> int:
-        return 3 * self._bow_level
-
-
-class Druid(Elf):
-    def __init__(self, nickname: str, musical_instrument: str, 
-                 favourite_spell: str) -> None:
-        super().__init__(nickname, musical_instrument)
-        self._favourite_spell = favourite_spell
-
-    def player_info(self) -> str:
-        return f'"Elf ranger {self.nickname}. {self.nickname} has a \
-            favourite spell: {self.favourite_spell}"` for `Druid` \
-            instances"'
-
-    def get_rating(self) -> int:
-        return len(self._favourite_spell)
-
-
-class DwarfWarrior(Dwarf):
-    def __init__(self, nickname: str, favourite_dish: str,
-                 hummer_level: int) -> None:
-        super().__init__(nickname, favourite_dish)
-        self._hummer_level = hummer_level
-
-    def player_info(self) -> str:
-        return f'"Dwarf warrior {self.nickname}. {self.nickname} has a hummer \
-            of the {self.hummer_level} level"` for `DwarfWarrior` instances'
-
-    def get_rating(self) -> int:
-        return self._hummer_level + 4
-
-
-class DwarfBlacksmith(Dwarf):
-    def __init__(self, nickname: str, favourite_dish: str, 
-                 skill_level: int) -> None:
-        super().__init__(nickname, favourite_dish)
-        self._skill_level = skill_level
-
-    def player_info(self) -> str:
-        return f'"Dwarf blacksmith {self.nickname} with skill of the \
-            {self.skill_level} level"` for `DwarfBlacksmith` instances'
-
-    def get_rating(self) -> int:
-        return self._skill_level
-
+from app.players.player import Player
+from app.players.dwarves.dwarf import Dwarf
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.elves.elf import Elf
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.druid import Druid
 
 def calculate_team_total_rating(players: list[Player]) -> int:
     sum_ratings = 0
---
+++ app/players/dwarves/dwarf_warrior.py
@@ -0,0 +1,12 @@
+class DwarfWarrior(Dwarf):
+    def __init__(self, nickname: str, favourite_dish: str,
+                 hummer_level: int) -> None:
+        super().__init__(nickname, favourite_dish)
+        self._hummer_level = hummer_level
+
+    def player_info(self) -> str:
+        return f'"Dwarf warrior {self.nickname}. {self.nickname} has a hummer \
+            of the {self.hummer_level} level"` for `DwarfWarrior` instances'
+
+    def get_rating(self) -> int:
+        return self._hummer_level + 4
