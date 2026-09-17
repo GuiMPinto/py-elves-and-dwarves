015107adfa242584e22455263de860314cdcb969
PintoJR <guimoraespinto@gmail.com>
2026-09-17 04:15:27 -0300

    Solution12

--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -9,7 +9,7 @@ class ElfRanger(Elf):
 
     def player_info(self) -> str:
         return (f"Elf ranger {self.nickname}. {self.nickname} has bow "
-        f"of the {self._bow_level} level")
+                f"of the {self._bow_level} level")
 
     def get_rating(self) -> int:
         return 3 * self._bow_level
