858b31de10d91d72b1b8f7a1f9bc0bb10a1c173b
PintoJR <guimoraespinto@gmail.com>
2026-09-17 02:56:22 -0300

    Solution09

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 from app.players.player import Player
 from app.players.dwarves.dwarf import Dwarf
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith #  noqa: F401
-from app.players.dwarves.dwarf_warrior import DwarfWarrior #  noqa: F401
 from app.players.elves.elf import Elf
-from app.players.elves.elf_ranger import ElfRanger #  noqa: F401
-from app.players.elves.druid import Druid #  noqa: F401
 
 
 
